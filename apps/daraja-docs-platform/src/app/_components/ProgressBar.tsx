6e7d5a718a739da70faecfc340af8dd3d35a82a1
Amos Kyalo <amoskyalo927@gmail.com>
2025-11-09 08:54:43 +0300

    add: progressbar

---
+++ apps/daraja-docs-platform/src/app/_components/ProgressBar.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import NProgress from 'nprogress';
+
+export default function ProgressBar() {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        NProgress.configure({
+            showSpinner: false,
+            trickleSpeed: 200,
+            minimum: 0.08,
+            easing: 'ease',
+            speed: 500
+        });
+    }, []);
+
+    useEffect(() => {
+        NProgress.done();
+    }, [pathname, searchParams]);
+
+    return null;
+}
